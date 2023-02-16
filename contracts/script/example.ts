9a32eb2bc0f4df97b592d4dc10e509f2a13855cb
Mehul Sharma <the8bitmehul@gmail.com>
2023-02-16 16:39:37 +0530

    Trigger events on basis of live matches

--- contracts/script/example.ts
+++ contracts/script/example.ts
@@ -1,6 +1,24 @@
-import { TonClient, WalletContractV3R2 } from 'ton'
-import { keyPairFromSecretKey } from 'ton-crypto'
-import { Event } from '../wrappers/Event'
+import axios from 'axios';
+import { keyPairFromSecretKey } from 'ton-crypto/dist/index';
+import {
+  TonClient,
+  WalletContractV3R2,
+} from 'ton/dist/index';
+
+import { Event } from '../wrappers/Event';
+
+const liveScoreBaseUrl = `https://livescore6.p.rapidapi.com/matches/v2`;
+const rapidApiKey = "07585b4120mshbc941a57c6ebd11p11de9bjsn089233df6ab2'"; 
+// As long as the repo is private this (^) is alright, should be using a process.env on production for this,
+// and squashing all commits before making repo public
+
+const Category = {
+  soccer: "soccer",
+  cricket: "cricket",
+  basketball: "basketball",
+  tennis: "tennis",
+  hockey: "hockey"
+}
 
 async function main () {
     // create 
@@ -27,7 +45,14 @@ async function main () {
 
     // Get the Event address
     const addr = event.address
+    
+    const category = Category.basketball;
 
+    const allMatches = await getAllMatchesByDate("20230312", category)
+    const matchEid: string = allMatches.Stages[0].Events[0].Eid as string;
+
+    const matchDetails = await getMatchDetails(matchEid, category);
+    
     // Start event
     await event.startEvent(sender)
 
@@ -37,6 +62,50 @@ async function main () {
     // true => outcome 1
     const result: boolean = false // outcome 0
     await event.finishEvent(sender, result)
+}
+
+const getAllMatchesByDate = async (date: string, category: string, timezone: number = 0-7) => {
+  /// This function is used to return all matches that 
+  /// happen on the date var,
+  /// the format of date is a "YYYYMMDD" since the api expects a string
+  
+  const options = {
+    method: 'GET',
+    url: `${liveScoreBaseUrl}/list-by-date`,
+    params: {Category: category, Date: date, Timezone: timezone.toString()},
+    headers: {
+      'X-RapidAPI-Key': `${rapidApiKey}`,
+      'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const resp = await axios.request(options)
+    return resp.data
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+const getMatchDetails = async (eid: string, category: string) => {
+  /// Returns a specific match's details
+
+  const options = {
+    method: 'GET',
+    url: `${liveScoreBaseUrl}/get-scoreboard`,
+    params: {Eid: eid, Category: category},
+    headers: {
+      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+      'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const resp = await axios.request(options)
+    return resp.data
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 main()
