a12bdcc5e93f77c1d3a7173f977e4f7d97273dc1
Daniil Sedov <42098239+Gusarich@users.noreply.github.com>
2023-02-26 11:56:43 +0300

    Update Bet.ts

--- contracts/wrappers/Bet.ts
+++ contracts/wrappers/Bet.ts
@@ -1,61 +1,75 @@
-import { Address, beginCell, Cell, Contract, Sender, toNano, TupleItem } from 'ton-core'
-import { compileFunc } from '@ton-community/func-js'
-import { readFileSync } from 'fs'
-import { ContractExecutor } from 'ton-emulator'
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    Sender,
+    toNano,
+    TupleItem,
+} from 'ton-core';
+import { compileFunc } from '@ton-community/func-js';
+import { readFileSync } from 'fs';
+import { ContractExecutor } from 'ton-emulator';
 
 export class Bet implements Contract {
-    readonly address: Address
-    readonly executor: ContractExecutor
+    readonly address: Address;
+    readonly executor?: ContractExecutor;
 
-    constructor (address: Address, executor: ContractExecutor) {
-        this.address = address
-        this.executor = executor
+    constructor(address: Address, executor?: ContractExecutor) {
+        this.address = address;
+        this.executor = executor;
     }
 
-    async close (via: Sender) {
+    async close(via: Sender) {
         await via.send({
             to: this.address,
             value: toNano('0.25'),
-            body: beginCell()
-                .storeUint(0x12d4de36, 32)
-            .endCell()
-        })
+            body: beginCell().storeUint(0x12d4de36, 32).endCell(),
+        });
     }
 
-    private async runGetMethod (method: string, stack?: TupleItem[] | undefined) {
-        var res = await this.executor.get(method)
-        if (!res.success) throw(res.error)
-        return res.stack
+    private async runGetMethod(
+        method: string,
+        stack?: TupleItem[] | undefined
+    ) {
+        var res = await this.executor!.get(method);
+        if (!res.success) throw res.error;
+        return res.stack;
     }
 
-    async getOwner () {
-        return (await this.runGetMethod('get_owner')).readAddress()
+    async getOwner() {
+        return (await this.runGetMethod('get_owner')).readAddress();
     }
 
-    async getEvent () {
-        return (await this.runGetMethod('get_event')).readAddress()
-    }
-    
-    async getOutcome () {
-        return (await this.runGetMethod('get_outcome')).readBoolean()
+    async getEvent() {
+        return (await this.runGetMethod('get_event')).readAddress();
     }
 
-    async getAmount () {
-        return (await this.runGetMethod('get_amount')).readBigNumber()
+    async getOutcome() {
+        return (await this.runGetMethod('get_outcome')).readBoolean();
     }
 
-    static async getCode (): Promise<Cell> {
+    async getAmount() {
+        return (await this.runGetMethod('get_amount')).readBigNumber();
+    }
+
+    static async getCode(): Promise<Cell> {
         let result = await compileFunc({
             targets: ['stdlib.fc', 'opcodes.fc', 'bet.fc'],
-            sources: (path) => readFileSync('func/' + path).toString()
-        })
+            sources: (path) => readFileSync('func/' + path).toString(),
+        });
         if (result.status === 'error') {
-            throw(result.message)
+            throw result.message;
         }
-        return Cell.fromBoc(Buffer.from(result.codeBoc, 'base64'))[0]
+        return Cell.fromBoc(Buffer.from(result.codeBoc, 'base64'))[0];
     }
 
-    static async getStateInit (owner: Address, event: Address, outcome: boolean, amount: bigint): Promise<{ code: Cell, data: Cell }> {
+    static async getStateInit(
+        owner: Address,
+        event: Address,
+        outcome: boolean,
+        amount: bigint
+    ): Promise<{ code: Cell; data: Cell }> {
         return {
             code: await this.getCode(),
             data: beginCell()
@@ -63,7 +77,7 @@ export class Bet implements Contract {
                 .storeAddress(event)
                 .storeBit(outcome)
                 .storeUint(amount, 256)
-            .endCell()
-        }
+                .endCell(),
+        };
     }
 }
